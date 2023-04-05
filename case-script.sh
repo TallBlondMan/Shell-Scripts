0524111e88d5c6e46e63908d97b0cf6e27792e5b
Mateusz <Mateusz.Smet@god.dev>
2023-04-05 10:21:45 +0000

    Case done

--- case-script.sh
+++ case-script.sh
@@ -16,8 +16,43 @@ case $EMOTION in
 			cats)
 				echo "Cats can jump 5 times their own height! :O"
 				;;
+			*)
+				echo "I don't know no $ANIMAL jokes :/"
+				;;
 		esac
 		;;
 	bad)
-		echo ""
+		echo "Ahhh, that's no good, wanna hear a joke?"
+		read JOKE
+		case $JOKE in
+			yes)
+				echo "Knock, knock..."
+				read ANSWR
+				case $ANSWR in 
+					"who's there?")
+						echo "Yo mama FAT and mine NTFS"
+						;;
+					*)
+						echo "Yo mood so bad you can't even answere correctly... good luck"
+						;;
+				esac
+				;;
+			no)
+				echo "ok."
+				;;
+			*)
+				echo "..."
+				;;
+		esac
+		;;
+	terrible)
+		echo "Damn Human, go here - https://www.recdit.com/r/aww and look at some kittens, I'll work for you"
+		;;
+	*)
+		echo "Emotion not recognized, try again"
+		;;
+esac
+echo "Program ended, have a great day!"
+
+
 
